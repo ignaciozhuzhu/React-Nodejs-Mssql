772a4ae406b5a00da391c156987640399658577e
zhus <ignacio3@126.com>
2017-03-22 10:37:50 +0800

    修改执行计划

--- server/postdemo/schedule.js
+++ server/postdemo/schedule.js
@@ -6,23 +6,31 @@ var fun_ghnext = require('./post_gh_del.js')
 var fun_yynext = require('./post_yy_del.js');
 
 var rule = new schedule.RecurrenceRule();
-//rule.second = 10;
-//每小时的30分执行
-//20分钟一次
-rule.minute = [10, 15, 20, 25, 30, 35, 40, 45, 50, 55];
+rule.dayOfWeek = [0, new schedule.Range(1, 6)];　　
+rule.hour = [];
+//凌晨2点到3点这块时间段留给ons计划使用.
+for (var i = 0; i < 24; i++) {
+	if (i != 2)
+		rule.hour.push(i)
+}
+rule.minute = [];
+//每分钟执行
+for (var i = 0; i < 60; i++) {
+	rule.minute.push(i)
+}
 var j = schedule.scheduleJob(rule, function() {
 	console.log("\n当前时间:" + getdate.fn(new Date()));
 	fun_ghnext.deleteNext();　
 	fun_yynext.deleteResNext();　　
 });
 
-var rule2 = new schedule.RecurrenceRule();
-//周一到周日的凌晨3点执行
-rule2.dayOfWeek = [0, new schedule.Range(1, 6)];　　
-rule2.hour = 2;
-rule2.minute = 00;
-rule2.second = 00;
-var j2 = schedule.scheduleJob(rule2, function() {
+var ruleons = new schedule.RecurrenceRule();
+//周一到周日的凌晨2点执行
+ruleons.dayOfWeek = [0, new schedule.Range(1, 6)];　　
+ruleons.hour = 2;
+ruleons.minute = 00;
+ruleons.second = 00;
+var jons = schedule.scheduleJob(ruleons, function() {
 	fun_gh.importDataBatch();
 	fun_yy.importDataResBatch();　
 });
