d611b4e80e9bc6587c829f70a7951fcf586a68e9
zhus <ignacio3@126.com>
2017-09-15 17:44:35 +0800

    .

--- server/postdemo/post_yy_del.js
+++ server/postdemo/post_yy_del.js
@@ -44,58 +44,60 @@ exports.deleteResAll = function(callbackfunction) {
 var Arraydata = [];
 //使用批量后,作为增量更新使用
 exports.deleteResNext = function() {
-    var count = 0;
-    fun.getHosDataOpeDelResnext(function(data) {
-        Arraydata = data;
+    conf.login(function() {
+        var count = 0;
+        fun.getHosDataOpeDelResnext(function(data) {
+            Arraydata = data;
 
-        function Data(url, body) {
-            var O = new Object();
-            O.headers = {
-                "Connection": "close"
-            };
-            O.url = url;
-            O.method = 'POST';
-            O.json = true;
-            O.body = body;
-            return O;
-        }
-
-        var options = [];
-        var importGH = function() {
-            for (var i = 0, len = Arraydata.length; i < len; i++) {
-                //从挂号到结束数据导入的接口,增加至可执行post对象中
-                options[i] = Data(conf.service + 'hosDataOpe/importReservation', Arraydata[i]);
+            function Data(url, body) {
+                var O = new Object();
+                O.headers = {
+                    "Connection": "close"
+                };
+                O.url = url;
+                O.method = 'POST';
+                O.json = true;
+                O.body = body;
+                return O;
             }
-        }
-        importGH();
-        console.log("aaaaa:" + JSON.stringify(options[0]))
-            //循环执行导入(批量导入)
-        try {
-            request(options[0], callback);
-        } catch (error) {}
 
-        function callback(error, response, data) {
-            //try {
-            if (!error && response.statusCode == 200) {
-                console.log('----info------\n'); //, data
-                count++;
-                if (count < options.length)
+            var options = [];
+            var importGH = function() {
+                for (var i = 0, len = Arraydata.length; i < len; i++) {
+                    //从挂号到结束数据导入的接口,增加至可执行post对象中
+                    options[i] = Data(conf.service + 'hosDataOpe/importReservation', Arraydata[i]);
+                }
+            }
+            importGH();
+            console.log("aaaaa:" + JSON.stringify(options[0]))
+                //循环执行导入(批量导入)
+            try {
+                request(options[0], callback);
+            } catch (error) {}
+
+            function callback(error, response, data) {
+                //try {
+                if (!error && response.statusCode == 200) {
+                    console.log('----info------\n'); //, data
+                    count++;
+                    if (count < options.length)
+                        request(options[count], callback);
+                } else if (response == undefined) {
+                    console.log("无数据");
+                } else {
+                    console.log('导入失败~~error:' + error + '~~data:' + data);
+                    count++;
                     request(options[count], callback);
-            } else if (response == undefined) {
-                console.log("无数据");
-            } else {
-                console.log('导入失败~~error:' + error + '~~data:' + data);
-                count++;
-                request(options[count], callback);
+                }
+
+                console.log("截止目前总共删除" + count + "条记录");
+                // } catch (error) {
+                if (count == options.length) {
+                    console.log("-----------终了-----------");
+                }
+                // }
             }
 
-            console.log("截止目前总共删除" + count + "条记录");
-            // } catch (error) {
-            if (count == options.length) {
-                console.log("-----------终了-----------");
-            }
-            // }
-        }
-
+        })
     })
 };
