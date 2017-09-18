629d24209de1a216385114d698f5ec1f2893aad3
zhus <ignacio3@126.com>
2017-09-18 14:02:16 +0800

    .

--- server/data/getHosDataOpeNew.js
+++ server/data/getHosDataOpeNew.js
@@ -47,7 +47,7 @@ exports.getReservation2 = function(callback, year, month) {
         request(localService +
             /*'/Keson_GetYYData?ReturnType=1&NumType=1&cValue=&cStartDate=' + year + '' + month + '01'
                        '&cEndDate=' + year + '' + month + '01'*/
-            '/Keson_GetYYData?ReturnType=1&NumType=1&cValue=&cStartDate=20170629&cEndDate=20170631',
+            '/Keson_GetYYData?ReturnType=1&NumType=1&cValue=&cStartDate=20170629&cEndDate=20170630',
             function(error, response, body) {
                 if (!error && response.statusCode == 200) {
                     var str = body;
