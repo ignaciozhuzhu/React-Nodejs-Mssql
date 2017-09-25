f4eb59f8133fc28f469bf9fb813abfb0b781523b
zhus <ignacio3@126.com>
2017-09-25 18:28:21 +0800

    .

--- server/data/getHosDataOpeNew.js
+++ server/data/getHosDataOpeNew.js
@@ -177,44 +177,44 @@ exports.patientSync = function() {
         if (!error && response.statusCode == 200) {
             console.log(body)
             var arr = JSON.parse(body);
-            if (arr.data.length > 0) {
-                //再调用2.1 得到病人在系统的唯一关键字,判断是否需要往科胜数据库插入新病人.
-                for (var i = 0; i < arr.data.length; i++) {
-                    request(localService + '/GetPatientGuid?ReturnType=1&NumType=1&cNo=' + arr.data[i].mobile + '&cName=' + arr.data[i].patientname + '', function(error, response, body, i) {
-                        return function() {
-                                console.log(i)
-                            }
-                            /*if (!error && response.statusCode == 200) {
-                                var str = subJson(body)
-                                var arrKs = JSON.parse(str);
-                                if (arrKs.cGuid == "") {
-                                    //是的话就调用2．7 病人信息写入方法
-                                    var newUuid = uuid();
-                                    console.log("arr:" + JSON.stringify(arr))
-                                    console.log("i:" + i)
-                                    var patientname = arr.data[i].patientname;
-                                    var gender = arr.data[i].gender == '1' ? '男' : '女';
-                                    var birthday = arr.data[i].birthday;
-                                    var idcard = arr.data[i].idcard;
-                                    var mobile = arr.data[i].mobile;
-                                    var otherphone = arr.data[i].otherphone;
-                                    var wx = arr.data[i].wx;
-                                    var address = arr.data[i].address;
-                                    var firstdate = arr.data[i].firstdate;
-                                    var type = arr.data[i].type;
-                                    var Hosp_no = arr.data[i].hospitalname == '天津市德倍尔口腔诊所' ? '001' : '002';
+            //if (arr.data.length > 0) {
+            //再调用2.1 得到病人在系统的唯一关键字,判断是否需要往科胜数据库插入新病人.
+            for (var i = 0; i < 2; i++) {
+                request(localService + '/GetPatientGuid?ReturnType=1&NumType=1&cNo=' + arr.data[i].mobile + '&cName=' + arr.data[i].patientname + '', function(error, response, body, i) {
+                    return function() {
+                            console.log("i:" + i)
+                        }
+                        /*if (!error && response.statusCode == 200) {
+                            var str = subJson(body)
+                            var arrKs = JSON.parse(str);
+                            if (arrKs.cGuid == "") {
+                                //是的话就调用2．7 病人信息写入方法
+                                var newUuid = uuid();
+                                console.log("arr:" + JSON.stringify(arr))
+                                console.log("i:" + i)
+                                var patientname = arr.data[i].patientname;
+                                var gender = arr.data[i].gender == '1' ? '男' : '女';
+                                var birthday = arr.data[i].birthday;
+                                var idcard = arr.data[i].idcard;
+                                var mobile = arr.data[i].mobile;
+                                var otherphone = arr.data[i].otherphone;
+                                var wx = arr.data[i].wx;
+                                var address = arr.data[i].address;
+                                var firstdate = arr.data[i].firstdate;
+                                var type = arr.data[i].type;
+                                var Hosp_no = arr.data[i].hospitalname == '天津市德倍尔口腔诊所' ? '001' : '002';
 
-                                    request(localService + '/Keson_PostPatientData_Add?ReturnType=1&cValue=' + newUuid + '&cPatNo=' + newUuid + '&cPatName=' + patientname + '&cGender=' + gender + '&cBirthDay=' + birthday + '&cId=' + idcard + '&cMobile=' + mobile + '&cTelephone=' + otherphone + '&cweixin=' + wx + '&cAddress1=' + address + '&cFirstdate=' + firstdate + '&cSource=牙艺平台&cType=' + type + '&cIntroducer=牙艺平台&Hosp_no=' + Hosp_no + '', function(error, response, body) {
-                                        if (!error && response.statusCode == 200) {
-                                            console.log('病人写入成功')
-                                        } else console.log(error);
+                                request(localService + '/Keson_PostPatientData_Add?ReturnType=1&cValue=' + newUuid + '&cPatNo=' + newUuid + '&cPatName=' + patientname + '&cGender=' + gender + '&cBirthDay=' + birthday + '&cId=' + idcard + '&cMobile=' + mobile + '&cTelephone=' + otherphone + '&cweixin=' + wx + '&cAddress1=' + address + '&cFirstdate=' + firstdate + '&cSource=牙艺平台&cType=' + type + '&cIntroducer=牙艺平台&Hosp_no=' + Hosp_no + '', function(error, response, body) {
+                                    if (!error && response.statusCode == 200) {
+                                        console.log('病人写入成功')
+                                    } else console.log(error);
 
-                                    })
-                                } else console.log("该牙艺新患者已在科胜库中.")
-                            } else console.log(error);*/
-                    }(i));
-                }
-            } else console.log('无最新患者')
+                                })
+                            } else console.log("该牙艺新患者已在科胜库中.")
+                        } else console.log(error);*/
+                }(i));
+            }
+            // } else console.log('无最新患者')
         } else console.log(error);
     })
 }
